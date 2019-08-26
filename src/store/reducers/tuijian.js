0f64a80b02daf13fd95dd6b5cbe5103c99c1049b
wari-1 <405770221@qq.com>
2019-08-26 16:47:11 +0800

    hgh

--- src/store/reducers/tuijian.js
+++ src/store/reducers/tuijian.js
@@ -7,11 +7,11 @@ const initialState = [
         id: "6t31yu2ui",
         src:
           "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/u166_02.jpg",
-          address: "清华大学艺术博物馆",
-          startTime: "2019-09-10T15:05:42+08:00",
-          endTime: "2019-11-09T15:05:42+08:00",
-          during: "2019.09.10-11.09",
-        isDisplaying: true,
+        address: "清华大学艺术博物馆",
+        startTime: "2019-09-10T15:05:42+08:00",
+        endTime: "2019-11-09T15:05:42+08:00",
+        during: "2019.09.10-11.09",
+        wen: false,
         desc:
           "本次展览将从人像摄影家肖全为杨丽萍拍摄的众多摄影中挑选出一组精品一个是舞之精灵,美的化身:一个被营为中国最好的人像摄影家。两人的心灵碰隆会擦出什么样的艺术火花?指影师镜头下直实的杨丽萍是什么样的?25日开始的“心动展,无疑极六地撩起了人们的好奇心",
         author: {
@@ -36,10 +36,304 @@ const initialState = [
         ],
         zanNum: 159,
         liulanNum: 394
+      },
+      {
+        id: "dwyi782ejd",
+        src:
+          "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/u166_04.jpg",
+        address: "尤伦斯艺术中心",
+        startTime: "2019-09-19T15:05:42+08:00",
+        endTime: "2019-11-09T15:05:42+08:00",
+        during: "2019.09.19-11.09",
+        wen: false,
+        desc:
+          "9月19日,中国当代艺术宗曾梵志迄今为止规模最大、涵盖最广泛的展览《散步》将在尤伦斯当代艺术中心举行。北京。本次展览由∪CCA馆长田霏宇与郭希联合策划,囊括以油彩、木材金属、纸本等材料创作的60余件作品,反映了艺术家近30年的持续性创作概貌",
+        author: {
+          desc:
+            "梵志,1964年出生于湖北武汉市,1991年毕业于煳北黑术学油画系。从二纪九十年代起,留梵志的创作以目油特的语离冈格和锐的社会批判界广泛的营,也得到社会民众的持续关注",
+          src:
+            "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/zeng_02.gif"
+        },
+        works: {
+          src: [
+            "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/zeng_04.gif",
+            "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/zeng_06.gif",
+            "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/zeng_08.gif",
+            "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/zeng_10.gif"
+          ]
+        },
+        comments: [
+          {
+            id: "h1hj",
+            text: "挺好的",
+            name: "阿德"
+          }
+        ],
+        zanNum: 1276,
+        liulanNum: 6712
+      },
+      {
+        id: "dwyi7y2167182ejd",
+        src:
+          "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/u166_06.jpg",
+        address: "东京都博物馆",
+        startTime: "2019-10-08T15:05:42+08:00",
+        endTime: "2019-11-09T15:05:42+08:00",
+        during: "2019.09.19-11.09",
+        wen: false,
+        desc:
+          "9月19日,中国当代艺术宗曾梵志迄今为止规模最大、涵盖最广泛的展览《散步》将在尤伦斯当代艺术中心举行。北京。本次展览由∪CCA馆长田霏宇与郭希联合策划,囊括以油彩、木材金属、纸本等材料创作的60余件作品,反映了艺术家近30年的持续性创作概貌",
+        author: {
+          desc:
+            "梵志,1964年出生于湖北武汉市,1991年毕业于煳北黑术学油画系。从二纪九十年代起,留梵志的创作以目油特的语离冈格和锐的社会批判界广泛的营,也得到社会民众的持续关注",
+          src:
+            "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/zeng_02.gif"
+        },
+        works: {
+          src: [
+            "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/zeng_04.gif",
+            "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/zeng_06.gif",
+            "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/zeng_08.gif",
+            "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/zeng_10.gif"
+          ]
+        },
+        comments: [
+          {
+            id: "h1hj",
+            text: "挺好的",
+            name: "阿德"
+          }
+        ],
+        zanNum: 277,
+        liulanNum: 1354
+      },
+      {
+        id: "dwyi7y2167182ejd",
+        src:
+          "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/u166_06.jpg",
+        address: "东京都博物馆",
+        startTime: "2019-10-08T15:05:42+08:00",
+        endTime: "2019-11-09T15:05:42+08:00",
+        during: "2019.09.19-11.09",
+        wen: false,
+        desc:
+          "9月19日,中国当代艺术宗曾梵志迄今为止规模最大、涵盖最广泛的展览《散步》将在尤伦斯当代艺术中心举行。北京。本次展览由∪CCA馆长田霏宇与郭希联合策划,囊括以油彩、木材金属、纸本等材料创作的60余件作品,反映了艺术家近30年的持续性创作概貌",
+        author: {
+          desc:
+            "梵志,1964年出生于湖北武汉市,1991年毕业于煳北黑术学油画系。从二纪九十年代起,留梵志的创作以目油特的语离冈格和锐的社会批判界广泛的营,也得到社会民众的持续关注",
+          src:
+            "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/zeng_02.gif"
+        },
+        works: {
+          src: [
+            "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/zeng_04.gif",
+            "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/zeng_06.gif",
+            "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/zeng_08.gif",
+            "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/zeng_10.gif"
+          ]
+        },
+        comments: [
+          {
+            id: "h1hj",
+            text: "挺好的",
+            name: "阿德"
+          }
+        ],
+        zanNum: 277,
+        liulanNum: 1354
+      },
+      {
+        id: "1666713",
+        src:
+          "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/u174_01.jpg",
+        address: "东京都博物馆",
+        startTime: "2019-10-08T15:05:42+08:00",
+        endTime: "2019-11-09T15:05:42+08:00",
+        during: "2019.09.19-11.09",
+        wen: false,
+        desc:
+          "9月19日,中国当代艺术宗曾梵志迄今为止规模最大、涵盖最广泛的展览《散步》将在尤伦斯当代艺术中心举行。北京。本次展览由∪CCA馆长田霏宇与郭希联合策划,囊括以油彩、木材金属、纸本等材料创作的60余件作品,反映了艺术家近30年的持续性创作概貌",
+        author: {
+          desc:
+            "梵志,1964年出生于湖北武汉市,1991年毕业于煳北黑术学油画系。从二纪九十年代起,留梵志的创作以目油特的语离冈格和锐的社会批判界广泛的营,也得到社会民众的持续关注",
+          src:
+            "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/zeng_02.gif"
+        },
+        works: {
+          src: [
+            "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/zeng_04.gif",
+            "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/zeng_06.gif",
+            "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/zeng_08.gif",
+            "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/zeng_10.gif"
+          ]
+        },
+        comments: [
+          {
+            id: "h1hj",
+            text: "挺好的",
+            name: "阿德"
+          }
+        ],
+        zanNum: 277,
+        liulanNum: 1354
+      },
+      {
+        id: "1666713",
+        src:
+          "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/u174_03.jpg",
+        address: "东京都博物馆",
+        startTime: "2019-10-08T15:05:42+08:00",
+        endTime: "2019-11-09T15:05:42+08:00",
+        during: "2019.09.19-11.09",
+        wen: false,
+        desc:
+          "9月19日,中国当代艺术宗曾梵志迄今为止规模最大、涵盖最广泛的展览《散步》将在尤伦斯当代艺术中心举行。北京。本次展览由∪CCA馆长田霏宇与郭希联合策划,囊括以油彩、木材金属、纸本等材料创作的60余件作品,反映了艺术家近30年的持续性创作概貌",
+        author: {
+          desc:
+            "梵志,1964年出生于湖北武汉市,1991年毕业于煳北黑术学油画系。从二纪九十年代起,留梵志的创作以目油特的语离冈格和锐的社会批判界广泛的营,也得到社会民众的持续关注",
+          src:
+            "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/zeng_02.gif"
+        },
+        works: {
+          src: [
+            "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/zeng_04.gif",
+            "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/zeng_06.gif",
+            "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/zeng_08.gif",
+            "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/zeng_10.gif"
+          ]
+        },
+        comments: [
+          {
+            id: "h1hj",
+            text: "挺好的",
+            name: "阿德"
+          }
+        ],
+        zanNum: 277,
+        liulanNum: 1354
       }
     ]
   },
-  { id: "hjhyew2223", wen: true, content: [] }
+  {
+    id: "hjhyew2223",
+    wen: true,
+    content: [
+      {
+        id: "1666713",
+        src:
+          "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/wen_03.png",
+        wen: true,
+        neirong: [
+          {
+            id: "12ye31rf",
+            src:
+              "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/shanzi_01.png",
+            desc: "木彳泽【中国百鬼】子不语怪力乱神。国人画之。"
+          },
+          {
+            id: "6721ey",
+            src:
+              "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/shanzi_03.png",
+            desc:
+              "【中国百鬼：」鬼夔卜九歌山鬼：“杳冥冥兮羌亘晦，东风飘兮神灵雨。留灵修兮澹忘归，岁既晏兮孰华予。“"
+          },
+          {
+            id: "7616gyu",
+            src:
+              "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/shanzi_05.png",
+            desc:
+              "狐骑・乘黄。《1海经・海外西经》：'白民之国在庞虽北,白首被发。有乘黄，0状如狐，目背上有角，乘之春二干岁。"
+          },
+          {
+            id: "172edhbc",
+            src:
+              "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/shanzi_07.png",
+            desc:
+              "【中国百鬼］逐愿讹兽。《神异经》：’西南荒中出讹兽，貝状若菟，人面能言，常欺人，言东而西，言恶而善。目肉美，食之，言不真矣。"
+          },
+          {
+            id: "6712edc",
+            src:
+              "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/shanzi_09.png",
+            desc:
+              "【中国百鬼】南国鮫人。晋干宝《搜神记》卷彳:1^)每之夕卜，有鮫人 水居如鱼"
+          },
+          {
+            id: "676t12ety",
+            src:
+              "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/shanzi_11.png",
+            desc: ""
+          }
+        ],
+        comments: [
+          {
+            id: "h1hj",
+            text: "挺好的",
+            name: "阿德"
+          }
+        ],
+        zanNum: 277,
+        liulanNum: 1354
+      },
+      {
+        id: "1666713",
+        src:
+          "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/wen_03.png",
+        wen: true,
+        neirong: [
+          {
+            id: "12ye31rf",
+            src:
+              "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/shanzi_01.png",
+            desc: "木彳泽【中国百鬼】子不语怪力乱神。国人画之。"
+          },
+          {
+            id: "6721ey",
+            src:
+              "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/shanzi_03.png",
+            desc:
+              "【中国百鬼：」鬼夔卜九歌山鬼：“杳冥冥兮羌亘晦，东风飘兮神灵雨。留灵修兮澹忘归，岁既晏兮孰华予。“"
+          },
+          {
+            id: "7616gyu",
+            src:
+              "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/shanzi_05.png",
+            desc:
+              "狐骑・乘黄。《1海经・海外西经》：'白民之国在庞虽北,白首被发。有乘黄，0状如狐，目背上有角，乘之春二干岁。"
+          },
+          {
+            id: "172edhbc",
+            src:
+              "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/shanzi_07.png",
+            desc:
+              "【中国百鬼］逐愿讹兽。《神异经》：’西南荒中出讹兽，貝状若菟，人面能言，常欺人，言东而西，言恶而善。目肉美，食之，言不真矣。"
+          },
+          {
+            id: "6712edc",
+            src:
+              "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/shanzi_09.png",
+            desc:
+              "【中国百鬼】南国鮫人。晋干宝《搜神记》卷彳:1^)每之夕卜，有鮫人 水居如鱼"
+          },
+          {
+            id: "676t12ety",
+            src:
+              "https://dev.tencent.com/u/dtid_30b2a4e50adc6692/p/images-tuoguan/git/raw/master/src/assets/images/shanzi_11.png",
+            desc: ""
+          }
+        ],
+        comments: [
+          {
+            id: "h1hj",
+            text: "挺好的",
+            name: "阿德"
+          }
+        ],
+        zanNum: 277,
+        liulanNum: 1354
+      }
+    ]
+  }
 ];
 export default (state = initialState, action) => {
   switch (action.type) {
