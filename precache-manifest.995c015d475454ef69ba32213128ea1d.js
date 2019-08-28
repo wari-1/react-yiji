5f56e9bc622b51d6dd37ccc8cf099260c86a8679
wari-1 <405770221@qq.com>
2019-08-28 22:46:58 +0800

    Updates

---
+++ precache-manifest.995c015d475454ef69ba32213128ea1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a0e7f700e9152c0eb98876428d41763",
+    "url": "/react-yiji/index.html"
+  },
+  {
+    "revision": "ac43b05e3bb00e530e6b",
+    "url": "/react-yiji/static/css/2.acb72abc.chunk.css"
+  },
+  {
+    "revision": "369821b509b3bda8bc6d",
+    "url": "/react-yiji/static/css/main.0c320026.chunk.css"
+  },
+  {
+    "revision": "ac43b05e3bb00e530e6b",
+    "url": "/react-yiji/static/js/2.fd383818.chunk.js"
+  },
+  {
+    "revision": "369821b509b3bda8bc6d",
+    "url": "/react-yiji/static/js/main.38cc8e55.chunk.js"
+  },
+  {
+    "revision": "76e3e25619ead56721a2",
+    "url": "/react-yiji/static/js/runtime~main.df17a495.js"
+  }
+]);
