4d322190c0e48e173dcd53cc39b593ac34dda487
wari-1 <405770221@qq.com>
2019-08-29 15:36:21 +0800

    Updates

--- precache-manifest.925a8603908e6475f1076ccf91d434b6.js
+++ precache-manifest.925a8603908e6475f1076ccf91d434b6.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f82bbc8da61d4750884ec8141dfaaa8b",
+    "revision": "7e2afac4de0b309c1407e17751215f31",
     "url": "/react-yiji/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-yiji/static/css/2.acb72abc.chunk.css"
   },
   {
-    "revision": "db06eeb48eb08a9a5289",
-    "url": "/react-yiji/static/css/main.40c0f5d4.chunk.css"
+    "revision": "eecdadb7595b36cbd2ae",
+    "url": "/react-yiji/static/css/main.c7ee8edc.chunk.css"
   },
   {
     "revision": "a6a5335109332ce82f1c",
     "url": "/react-yiji/static/js/2.389b4ed7.chunk.js"
   },
   {
-    "revision": "db06eeb48eb08a9a5289",
-    "url": "/react-yiji/static/js/main.88648b08.chunk.js"
+    "revision": "eecdadb7595b36cbd2ae",
+    "url": "/react-yiji/static/js/main.5751b3fe.chunk.js"
   },
   {
     "revision": "76e3e25619ead56721a2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-yiji/precache-manifest.93b2bc805252aa94b1904fea079c2e76.js"
+  "/react-yiji/precache-manifest.925a8603908e6475f1076ccf91d434b6.js"
 );
 
 self.addEventListener('message', (event) => {
